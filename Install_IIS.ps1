6a61f6dec21891ac18ceb1adee51822acee31472
Edoardo Sanna <Edoardo.sanna@gmx.com>
2020-02-13 20:38:48 +0100

    Create Install_IIS.ps1

---
+++ Install_IIS.ps1
@@ -0,0 +1,72 @@
+# Source: https://weblog.west-wind.com/posts/2017/may/25/automating-iis-feature-installation-with-powershell
+# This script installs IIS and the features required to run our web application
+#
+# Tested on PowerShell 5.1
+
+# * Make sure you run this script from a Powershel Admin Prompt!
+# * Make sure Powershell Execution Policy is bypassed to run these scripts:
+# * YOU MAY HAVE TO RUN THIS COMMAND PRIOR TO RUNNING THIS SCRIPT!
+Set-ExecutionPolicy Bypass -Scope Process
+
+# To list all Windows Features: 
+# dism /online /Get-Features
+# or:
+# Get-WindowsOptionalFeature -Online 
+# LIST All IIS FEATURES: 
+# Get-WindowsOptionalFeature -Online | where FeatureName -like 'IIS-*'
+
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServer
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-CommonHttpFeatures
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpErrors
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpRedirect
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationDevelopment
+# Watch out for these two:
+Enable-WindowsOptionalFeature -online -FeatureName NetFx4Extended-ASPNET45
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-NetFxExtensibility45
+
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HealthAndDiagnostics
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpLogging
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-LoggingLibraries
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestMonitor
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpTracing
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-Security
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestFiltering
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-Performance
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerManagementTools
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-IIS6ManagementCompatibility
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-Metabase
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ManagementConsole
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-BasicAuthentication
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WindowsAuthentication
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-StaticContent
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-DefaultDocument
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebSockets
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationInit
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIExtensions
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIFilter
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpCompressionStatic
+
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45
+
+# If you need classic ASP (not recommended)
+#Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASP
+
+# WebAdministration module
+Import-Module WebAdministration
+
+# To create an Application Pool
+New-WebAppPool -name "NewWebSiteAppPool"  -force
+
+# Reconfiguring the new Application Pool
+# Source: https://docs.microsoft.com/en-us/iis/configuration/system.applicationhost/applicationpools/add/processmodel
+$appPool = Get-Item IIS:\AppPools\NewWebSiteAppPool 
+# Identity type
+$appPool.processModel.identityType = "ApplicationPoolIdentity"
+# Enable 32-bit applications
+$appPool.enable32BitAppOnWin64 = 1
+# Set processModel.idleTimeout to 8 hours
+$appPool.processModel.idleTimeout = "08:00:00"
+# Apply previous changes
+$appPool | Set-Item
+
