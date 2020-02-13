a34ff18f6a834414cee0386d49a68e75cecfeca0
Edoardo Sanna <Edoardo.sanna@gmx.com>
2020-02-13 22:32:35 +0100

    Update and rename Install_IIS.ps1 to IIS_install_and_configure.ps1

--- _automate/IIS_install_and_configure.ps1
+++ _automate/IIS_install_and_configure.ps1
@@ -8,7 +8,6 @@
 # * YOU MAY HAVE TO RUN THIS COMMAND PRIOR TO RUNNING THIS SCRIPT!
 Set-ExecutionPolicy Bypass -Scope Process
 
-
 # To begin with: Creating and updating a log file with timestamps
 $Logpath = "C:\_TEMP"
 $Logfile = "$Logpath\IIS_install.log"
@@ -20,13 +19,15 @@ Function LogWrite
 }
 LogWrite "Start logging"
 
-
+# ---------------------------------------------------- 
+# Install IIS
+#
 # To list all Windows Features: 
-# dism /online /Get-Features
+# 	dism /online /Get-Features
 # or:
-# Get-WindowsOptionalFeature -Online 
+# 	Get-WindowsOptionalFeature -Online 
 # LIST All IIS FEATURES: 
-# Get-WindowsOptionalFeature -Online | where FeatureName -like 'IIS-*'
+# 	Get-WindowsOptionalFeature -Online | where FeatureName -like 'IIS-*'
 
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServer
@@ -59,56 +60,67 @@ Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationInit
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIExtensions
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIFilter
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpCompressionStatic
-
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45
-
 # If you need classic ASP (not recommended)
 #Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASP
 
-# Import WebAdministration module
-# It depends on the OS version, maybe insert a check with:
-(Get-WMIObject win32_operatingsystem).name
+# ---------------------------------------------------- 
+# Insert mpassw setup here
+
+# ----------------------------------------------------
+# IIS Configuration
+
+# Global variables
+$ApplicationPoolName = "MICRONTEL_Accessi"
+$WebSiteName = "Default Web Site"
+$ApplicationName = "/mpassw"
+# Get IIS Version and import admin modules
+$IISVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo(“C:\Windows\system32\notepad.exe”).FileVersion
 Import-Module WebAdministration -ErrorAction SilentlyContinue # For IIS 7.5 (Windows Server 2008 R2 on)
 Import-Module IISAdministration # For IIS 10.0 (Windows Server 2016 and 2016-nano on)
+$manager = Get-IISServerManager
 
-# To create an Application Pool
-New-WebAppPool -name "NewWebSiteAppPool"  -force
-
-# Reconfiguring the new Application Pool
-# Source: https://docs.microsoft.com/en-us/iis/configuration/system.applicationhost/applicationpools/add/processmodel
-$appPool = Get-Item IIS:\AppPools\NewWebSiteAppPool 
-# Identity type
-$appPool.processModel.identityType = "ApplicationPoolIdentity"
-# Enable 32-bit applications
-$appPool.enable32BitAppOnWin64 = 1
-# Set processModel.idleTimeout to 8 hours
-$appPool.processModel.idleTimeout = "08:00:00"
-# Apply previous changes
-$appPool | Set-Item
-
-
-
-# Check if IIS is installed, and which version
-# Still to be tested!
-# -----------------------------
-try {
-  # Is IIS installed?
-	$iisFeature = Get-WindowsFeature Web-WebServer -ErrorAction Stop
-	if ($iisFeature -eq $null -or $iisFeature.Installed -eq $false) {
-    # IIS not installed, print error
-		Write-Error "It looks like IIS is not installed on this server and the deployment is likely to fail."
-		Write-Error "Tip: You can use PowerShell to ensure IIS is installed: 'Install-WindowsFeature Web-WebServer'"
-		Write-Error "     You are likely to want more IIS features than just the web server. Run 'Get-WindowsFeature *web*' to see all of the features you can install."
-		exit 1
-	}
-	else {
-    # IIS installed, get version from Registry Key
-		$iisVersion = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\InetStp\  | Select VersionString
-		Write-Host "Detected IIS $($iisVersion.VersionString)"
-	}
-} catch {
-  # IIS not installable
-	Write-Host "Call to `Get-WindowsFeature Web-WebServer` failed."
-	Write-Host "Unable to determine if IIS is installed on this server but will optimistically continue."
+# Create application pool, integrated pipeline, Runtime v4.0, Enable32bitApps, idleTimeout 8hrs
+# Using IISAdministration (IIS 10.0)
+if ($IISVersion.Substring(0,2) = '10') {
+	if ($manager.ApplicationPools["$ApplicationPoolName"] -eq $null) {
+	$pool = $manager.ApplicationPools.Add("$ApplicationPoolName")
+	$pool.ManagedPipelineMode = "Integrated"
+	$pool.ManagedRuntimeVersion = "v4.0"
+	$pool.Enable32BitAppOnWin64 = $true
+	$pool.AutoStart = $true
+	$pool.ProcessModel.IdentityType = "ApplicationPoolIdentity"
+	$pool.ProcessModel.idleTimeout = "08:00:00"
+	$manager.CommitChanges()
+	LogWrite "Application Pool '$ApplicationPoolName' successfully created"
+	} else {LogWrite "Application Pool '$ApplicationPoolName' already exists, please choose a different name"}
+} 
+# On WebAdministration (IIS 7.5)
+else {
+	if ((Test-Path "IIS:\AppPools\$ApplicationPoolName") -eq $False) {
+	New-WebAppPool -name "$ApplicationPoolName"  -force
+	$appPool = Get-Item IIS:\AppPools\$ApplicationPoolName 
+	$appPool.processModel.identityType = "ApplicationPoolIdentity"
+	$appPool.enable32BitAppOnWin64 = 1
+	$appPool.processModel.idleTimeout = "08:00:00"
+	$appPool | Set-Item
+	LogWrite "Application Pool '$ApplicationPoolName' successfully created"
+	} else {LogWrite "Application Pool '$ApplicationPoolName' already exists, please choose a different name"}
 }
 
+# Assign the web application mpassw to the application pool
+# Using IISAdministration (IIS 10.0)
+if ($IISVersion.Substring(0,2) = '10') {
+	$website = $manager.Sites["$WebSiteName"]
+	$website.Applications["$ApplicationName"].ApplicationPoolName = "$ApplicationPoolName"
+	$manager.CommitChanges()
+	LogWrite "Application '$WebSiteName$ApplicationName' successfully assigned to Application pool '$ApplicationPoolName'"
+}
+# Using WebAdministration (IIS 7.5)
+else {
+	Set-ItemProperty -Path "IIS:\Sites\$WebSiteName\$ApplicationName" -name "applicationPool" -value "$ApplicationPoolName"
+	LogWrite "Application '$WebSiteName$ApplicationName' successfully assigned to Application pool '$ApplicationPoolName'"
+}
+
+# Next steps: enabling application-level logging
+# Next steps: add authentication modes as parameters
