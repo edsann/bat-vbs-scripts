546a3e4eb2d0fdc279cb4b48a7441f24b3ea1b58
Edoardo Sanna <Edoardo.sanna@gmx.com>
2020-03-02 16:19:40 +0100

    Update Get_configuration.ps1

--- _automate/Get_configuration.ps1
+++ _automate/Get_configuration.ps1
@@ -1,8 +1,11 @@
-# Convert app config file into XML format 
+#Translated XML config
 $xml = [xml] (Get-Content "C:\MPW\MicronConfig\config.exe.config")
 
-# Read value from dbengine and print it on file
-$xml.SelectSingleNode('//add[@key="dbEngine"]').Value | Out-File "C:\MPW\temp.txt" -append
+# Read value from dbengine
+$dbengine = $xml.SelectSingleNode('//add[@key="dbEngine"]').Value
+$mydbengine = "$("//add[@key='")$($dbengine)$("Str']")"
 
 # Read value from SqlStr and print it on file
-$xml.SelectSingleNode('//add[@key="SqlStr"]').Value | Out-File "C:\MPW\temp.txt" -append
+$connectionstring = $xml.SelectSingleNode($mydbengine).Value | Out-File "C:\MPW\temp.txt" -append
+
+#write-output $connectionstring
