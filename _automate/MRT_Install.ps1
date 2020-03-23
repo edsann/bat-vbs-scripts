0e19668348476bef53cb15f51618fdb859cf7331
edsann <edoardo.sanna@gmx.com>
2020-03-23 17:34:42 +0100

    Update MRT_Install.ps1

--- _automate/MRT_Install.ps1
+++ _automate/MRT_Install.ps1
@@ -1,10 +1,15 @@
 cd C:\MPW
 # Create package msi in current dir
 ./mrt7526.exe /s /x /b"$PWD" /v"/qn"
-# Silently install msi
-& "msiexec.exe" /qn /i 'Micronpass Application Suite.msi'
+# Silently install msi (cmd) and create low-error log (run as admin!)
+$msiArguments = 
+    '/qn', 
+    '/i',
+    '"Micronpass Application Suite.msi"',
+    '/le "C:\MPW\MRT_setup.log"'
+$Process = Start-Process -PassThru -Wait msiexec -ArgumentList $msiArguments
 
-# Check if everything has been correctly installed
+# Check if everything has been correctly installed (still WIP)
+if ($Process.ExitCode -eq "0") {"Successfully installed"}
 if (Test-Path C:\MPW) {"MPW folder exists"}
 if (get-service -name btService) {"btService exists"}
-# work-in-progress
