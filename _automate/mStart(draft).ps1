929876bfc3f14c2836b945e9f7f6e171ac8175f2
edsann <edoardo.sanna@gmx.com>
2020-03-30 17:49:32 +0200

    Update mStart(draft).ps1

--- _automate/mStart(draft).ps1
+++ _automate/mStart(draft).ps1
@@ -1,21 +1,22 @@
-﻿cls
+﻿# Open GeneraABL
 cd C:\MPW\GeneraAbl\
 Start-process ./GeneraAbl.exe 
 
 if (
-# Systeminfo --> System Model = Physical 
-# get-wmiobject win32_computersystem | fl model
+    # Systeminfo --> System Model = Physical 
+    # get-wmiobject win32_computersystem | fl model
 ){
-$exec = "{TAB}{ENTER}"
+    $keys = "{TAB}{ENTER}"
 } else {
-# Systeminfo --> System Model = Virtual 
-# get-wmiobject win32_computersystem | fl model
-$exec = "{TAB}{ENTER}{ENTER}"
+    # Systeminfo --> System Model = Virtual 
+    # get-wmiobject win32_computersystem | fl model
+    $keys = "{TAB}{TAB}{ENTER}"
 }
 $wshshell = New-Object -ComObject WScript.Shell
 Start-Sleep -Seconds 1
-$wshshell.sendkeys($exec )
+$wshshell.sendkeys($keys)
 
+# Open MicronStart and wait for input
 cd C:\MPW\MicronStart
 Start-process ./mStart.exe -Wait
 Write-Host "Going on..."
