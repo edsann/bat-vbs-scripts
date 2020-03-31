e02d76cf7576b0507591b2e8c39b052e9cdc2e2e
edsann <edoardo.sanna@gmx.com>
2020-03-31 20:26:10 +0200

    Update IIS_MRT_install.ps1

--- _automate/IIS_MRT_install.ps1
+++ _automate/IIS_MRT_install.ps1
@@ -127,10 +127,11 @@ if ($(get-wmiobject win32_computersystem).model -match "virtual,*"){
     $keys = "{TAB}{ENTER}"
 }
 $wshshell = New-Object -ComObject WScript.Shell
-Start-Sleep -Seconds 1
+Start-Sleep -Seconds 2
 $wshshell.sendkeys($keys)
 
 # Open MicronStart and wait for input
+Start-sleep -Seconds 5
 cd C:\MPW\MicronStart
 Start-process ./mStart.exe -Wait
 Write-Host "Going on..."
