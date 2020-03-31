76c9e3b4ecb27863ed5e11379c426328696d5c98
edsann <edoardo.sanna@gmx.com>
2020-03-31 15:10:26 +0200

    Tested on Serv2019

--- _automate/IIS_MRT_install.ps1
+++ _automate/IIS_MRT_install.ps1
@@ -2,28 +2,27 @@
 .SYNOPSIS
     Automate IIS installation on Windows client or server
     Automate MRT Application Suite installation
+.TESTED ON
+    See EOF
 .INPUT
     CSV file with required IIS features
     MRTxxx.exe in same directory
 .NOTE
-    Tested on Windows Server 2016 Datacenter
-    Tested on Windows Server 2019 Datacenter
-    To be tested on Windows 10 Pro build 1809
-    .Re-test all IIS features on Client
-    
-    .Add Set-ExecutionPolicy Unrestricted
-        Set-Executionpolicy -ExecutionPolicy Unrestricted -Force -ErrorAction SilentlyContinue 
-    .CheckIf-Installed is getting ugly, clean it up!
-    ..Add a little more Write-Host
+    .Manually re-test all IIS features on Client
+    .Clean up IIS installation function
     ..Add speed-test
+    ..Add DSC test at the end of the script
 #>
+
+Set-Executionpolicy -ExecutionPolicy Unrestricted -Force -ErrorAction SilentlyContinue 
 
 # Function: Writes a Log
 Function LogWrite {
    Param ([string]$logstring)
-   $LogPath = ".\Install.log"
+   $LogPath = ".\install.log"
    $datetime = Get-Date -format "[dd-MM-yyyy HH:mm:ss]"
    Add-content $LogPath -value "$datetime $logstring "
+   Write-Host $logstring
 }
 
 # Check if the current user is Administrator
@@ -33,78 +32,67 @@ Function Check-IsAdmin {
     $principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator) 
 }
 
-# Check if feature installation is successful
-function CheckIf-Installed($installedfeature,$command,$result) {
-    if ($installedfeature.$command -eq $result){
-        LogWrite "Windows Feature $installedfeature successfully installed"
-    } else {
-        LogWrite "ERROR - Something went wrong installing $feature, please check again!"
-        # Exit installation at the first error
-	    Exit
-    }
-}
-
-# Function: Check if program is installed, based on Name
-Function Check_Program_Installed($programName) {
-    $Program = Get-WMIObject -Query "SELECT * FROM Win32_Product WHERE Name LIKE '%$programName%'"
-    $wmi_check = $Program -ne $null
-    return $wmi_check;
-}
-
 <# ------------------------------------ #>
 
-LogWrite "1. Starting installation of IIS Web Server"
+LogWrite "1. Check environment info"
 
 # Check if current user is Administrator
 If (!( Check-IsAdmin) ) {
-    LogWrite "ERROR - The currently logged on user is not an Administrator! Exiting..."
+    LogWrite "ERROR - The currently logged on user is not an Administrator!"
     exit 
 }
 
-# Check OS type
-LogWrite "Checking OS infos..."
+# Check current execution policy
+If (Get-ExecutionPolicy -neq "Unrestricted" ) {
+    LogWrite "ERROR - The Execution Policies on the current session prevents this script to operate!"
+    exit 
+}
+
+# Check OS type 
 $OSDetails = Get-ComputerInfo
 $OSName = (Get-CimInstance -ClassName Win32_OperatingSystem).Caption
 $OSType = $OSDetails.WindowsInstallationType
-LogWrite "This is $OSName, so we're on a $OSType machine!"
 
-# IIS Features loaded from CSV file
-LogWrite "Gathering IIS specs..."
+# Load IIS Features from CSV file
 $IISFeaturesList = @(Import-CSV ".\IIS_features.csv" -Delimiter ';' -header 'FeatureName','Client','Server')
 $IISFeaturesList = $IISFeaturesList.$OSType
 
-LogWrite "Installing IIS features..."
+LogWrite "2. Install IIS features"
+
 # Workstation (DISM installation module)
 if ($OSType -eq "Client"){
-    $command = "State"
-    $result = "Enabled"
     foreach ($feature in $IISFeaturesList){
         Enable-WindowsOptionalFeature -Online -FeatureName $feature | Out-Null
-        $installedfeature = (Get-WindowsOptionalFeature -Online -FeatureName $feature).featureName
-        CheckIf-Installed($installedfeature,$command,$result)
+        if ((Get-WindowsOptionalFeature -Online -FeatureName $feature).State -eq "Enabled"){
+            LogWrite "Windows Feature $feature successfully installed"
+        } else {
+            LogWrite "ERROR - Something went wrong installing $feature, please check again!"
+	        Exit
         }
+    }
 } 
 # Server (ServerManager installation module)
 elseif ($OSType -eq "Server"){
-    $command = "Installed"
-    $result = $True
     foreach ($feature in $IISFeaturesList){
-        Install-WindowsFeature -Name $feature -ErrorAction SilentlyContinue | Out-Null
-        $installedfeature = Get-WindowsFeature -name $feature
-        CheckIf-Installed($installedfeature,$command,$result)
+        Install-WindowsFeature -Name $feature  | Out-Null
+        if ((Get-WindowsFeature -name $feature).Installed -eq $True){
+            LogWrite "Windows Feature $feature successfully installed"
+        } else {
+            LogWrite "ERROR - Something went wrong installing $feature, please check again!"
+	        Exit
         }
+    }
 }
 
 # Reset IIS
-LogWrite "Resetting IIS..."
-Invoke-Command -ScriptBlock { iisreset} -Verbose
-# Get IIS version
+Invoke-Command -ScriptBlock { iisreset} -Verbose | LogWrite
 $IISVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo(“C:\Windows\system32\notepad.exe”).FileVersion
 LogWrite "IIS $IISVersion successfully installed!"
 
 <# ------------------------------------ #>
 
-LogWrite "2. Installing MRT Application Suite..."
+LogWrite "3. Install MRT Application Suite..."
+
 # Create package msi in current dir
 ./mrt7526.exe /s /x /b"$PWD" /v"/qn"
 # Wait for extraction
@@ -117,13 +105,17 @@ $msiArguments =
     '/le ".\MRT_install.log"'
 $Process = Start-Process -PassThru -Wait msiexec -ArgumentList $msiArguments
 # Check if installation was successful
-if (($Process.ExitCode -eq '0') -and (Check_Program_Installed("Micronpass Application Suite") -eq $True )) {
+$Program = Get-WMIObject -Query "SELECT * FROM Win32_Product WHERE Name LIKE '%$programName%'"
+$wmi_check = $Program -ne $null
+if (($Process.ExitCode -eq '0') -and ($wmi_check -eq $True )) {
     LogWrite "MRT Application Suite $($Program.Version) successfully installed!"
 } Else {
     LogWrite "ERROR - Something went wrong installing MRT Application Suite, please check MRT_Install.log"
 }
 
 <# ------------------------------------ #>
+
+LogWrite "4. Activating product"
 
 # Open GeneraABL
 cd C:\MPW\GeneraAbl\
@@ -142,3 +134,9 @@ $wshshell.sendkeys($keys)
 cd C:\MPW\MicronStart
 Start-process ./mStart.exe -Wait
 Write-Host "Going on..."
+
+
+<#
+.TESTED ON
+    Windows Server 2019
+#>
