1d6dac09fa04ea7596dd36ab38c044a7ee04a744
Edoardo Sanna <edoardo.sanna89@gmail.com>
2020-07-20 17:33:18 +0200

    Some instructions to start

--- docker-test.ps1
+++ docker-test.ps1
@@ -2,3 +2,19 @@
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
 Install-Package -Name docker -ProviderName DockerMsftProvider
 Restart-Computer -Force
+
+# Run container
+docker run --name CONTAINER_NAME IMAGE
+
+# Rename container
+docker rename CONTAINER_NAME MY_NEW_CONTAINER_NAME
+
+# Get container ID
+docker ps -aqf "name=MY_NEW_CONTAINER_NAME"
+
+# Run a container after getting its ID
+docker start $(docker ps -aqf "name=MY_NEW_CONTAINER_NAME")
+
+# Pull container ASP.NET 4.8
+docker pull mcr.microsoft.com/dotnet/framework/aspnet:4.8
+
