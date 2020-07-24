5fe7e202c889455903c71b611ae9dcd8d9eb53cf
Edoardo Sanna <edoardo.sanna89@gmail.com>
2020-07-24 15:23:09 +0200

    Update docker-test.ps1

--- docker-test.ps1
+++ docker-test.ps1
@@ -3,13 +3,31 @@ Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
 Install-Package -Name docker -ProviderName DockerMsftProvider
 Restart-Computer -Force
 
-# Run (pull, create & start) container
-# Name it
-# Publish container port 80 on host port 8080
-docker run --name ASPNET48 --publish 8080:80 mcr.microsoft.com/dotnet/framework/aspnet:4.8
+# Pull ASP.NET 4.8 base image
+Docker pull mcr.microsoft.com/dotnet/framework/aspnet:4.8
 
 # Rename image
-# docker tag mcr.microsoft.com/dotnet/framework/aspnet:4.8 aspnet48baseimage
+docker tag mcr.microsoft.com/dotnet/framework/aspnet:4.8 aspnet48baseimage
+
+# ./Dockerfile
+  FROM aspnet48baseimage
+  WORKDIR /inetpub/wwwroot
+  COPY . /inetpub/wwwroot
+  RUN icacls c:/inetpub/wwwroot /grant Everyone:F
+  RUN dir 
+
+# Build new image
+docker build -t ASPNET48 .
+
+# Run (pull, create & start) container
+docker run --name ASPNET48 --publish 8080:80 mcr.microsoft.com/dotnet/framework/aspnet:4.8
+
+# Start PowerShell on running container
+docker exec -it ASPNET48 powershell
+
+
+
+# ----------------
 
 # Rename container
 # docker rename CONTAINER_NAME MY_NEW_CONTAINER_NAME
@@ -17,11 +35,4 @@ docker run --name ASPNET48 --publish 8080:80 mcr.microsoft.com/dotnet/framework/
 # Run a container after getting its ID
 # docker start $(docker ps -aqf "name=ASPNET48")
 
-# Dockerfile to grant access to inetpub/wwwroot
-  FROM aspnet48baseimage
-  WORKDIR /inetpub/wwwroot
-  COPY . /inetpub/wwwroot
-  # Give Full Access To Folder
-  RUN icacls 'c:/inetpub/wwwroot' /grant 'Everyone:(OI)(CI)F'
-  # Check that the files have been successfully copied
-  RUN dir 
+
