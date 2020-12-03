d64aa0f0d7d482cec89c738450ddd846b1580016
Edoardo Sanna <edoardo.sanna.es@gmail.com>
2020-12-03 08:43:28 +0100

    Create Set-ConsoleStringColor.ps1

---
+++ Set-ConsoleStringColor.ps1
@@ -0,0 +1,5 @@
+# List the colors available in Powershell
+[Enum]::GetValues([System.ConsoleColor])
+
+# Change the color from default to DarkRed of a specific property in the console (e.g. strings)
+Set-PSReadLineOption -TokenKind String -ForegroundColor DarkRed
